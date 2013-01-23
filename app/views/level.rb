eedff74a534cb6e570d530242755ab351cbee2bb
Sumeet Singh <ortuna@gmail.com>
2013-01-22 20:58:55 -0500

    Refactoring

--- app/views/level.rb
+++ app/views/level.rb
@@ -1,12 +1,16 @@
 class Level < UIScrollView
   def initWithFrame(*args)
     super
-    levelSetup
-
-    @world = World.new
-    @world.setupWorld(self.contentSize.width, height: self.contentSize.height)
-
     @box_pixel_scale = 24
+    setupLevel
+    setupWorld
+    createBoxes
+    scrollToBottom
+    setupTimer
+    self
+  end
+
+  def createBoxes
     (1..10).each do |item|
         width = Random.rand(3) + 1
         height = Random.rand(3) + 1
@@ -17,22 +21,34 @@ class Level < UIScrollView
         self.addSubview @blue_view
         @world.addBox @blue_view
     end
-    @timer = NSTimer.scheduledTimerWithTimeInterval(1.0/60.0,
-      target:self,
-      selector:'timeHandler:',
-      userInfo:nil,
-      repeats:true
-    )
-
-    self
   end
 
   def timeHandler(sender)
     @world.tick
   end
 
-  def levelSetup
-    size = CGSizeMake(UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height+1000)
+  def setupWorld
+    @world = World.new
+    @world.setupWorld(self.contentSize.width, height: self.contentSize.height)
+  end
+  def setupTimer
+    @timer = NSTimer.scheduledTimerWithTimeInterval(1.0/60.0,
+      target:self,
+      selector:'timeHandler:',
+      userInfo:nil,
+      repeats:true
+    )
+  end
+
+  def scrollToBottom
+    bottomOffset = CGPointMake(0, self.contentSize.height-self.bounds.size.height)
+    self.setContentOffset(bottomOffset, anitmated: false)
+  end
+
+  def setupLevel
+    width  = UIScreen.mainScreen.bounds.size.width
+    height = UIScreen.mainScreen.bounds.size.height * 3
+    size   = CGSizeMake(width, height)
     self.setContentSize(size)
   end
 
